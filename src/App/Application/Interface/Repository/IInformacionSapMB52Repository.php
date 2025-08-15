aa16360f45029b14bf4bee4a4eea051eaa6f6eec
ScanoHWI <administrador.smartcenter@hacebwhirlpool.com>
2025-08-15 13:13:50 -0500

    Archivos restaurados de nuevo

---
+++ src/App/Application/Interface/Repository/IInformacionSapMB52Repository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Application\Interface\Repository;
+
+use App\Domain\Model\InformacionSapMB52;
+
+interface IInformacionSapMB52Repository
+{
+    public function save(InformacionSapMB52 $informacion): bool;
+    
+}
---
+++ src/App/Infrastructure/Repository/InformacionSapMB52Repository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Infrastructure\Repository;
+
+use App\Domain\Model\InformacionSapMB52;
+use App\Application\Interface\Repository\IInformacionSapMB52Repository;
+use PDO;
+
+class InformacionSapMB52Repository implements IInformacionSapMB52Repository
+{
+    private $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+
+    public function save(InformacionSapMB52 $informacion): bool
+    {
+        $data = $informacion->toArray();
+
+        $columnas = implode(', ', array_keys($data));
+        $placeholders = ':' . implode(', :', array_keys($data));
+
+        $query = "INSERT INTO inventario_hwi_informacion_sap_mb52 ($columnas) VALUES ($placeholders)";
+        $stmt = $this->db->prepare($query);
+
+        foreach ($data as $campo => $valor) {
+            $stmt->bindValue(":$campo", $valor);
+        }
+
+        return $stmt->execute();
+    }
+}
