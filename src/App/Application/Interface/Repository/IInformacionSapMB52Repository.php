2bdfd5d73227535cef934ca840dfb92eada7ce44
ScanoHWI <administrador.smartcenter@hacebwhirlpool.com>
2025-08-15 12:55:52 -0500

    Confirmar cambios mb52

--- src/App/Application/Interface/Repository/IInformacionSapMB52Repository.php
+++ src/App/Application/Interface/Repository/IInformacionSapMB52Repository.php
@@ -7,4 +7,5 @@ use App\Domain\Model\InformacionSapMB52;
 interface IInformacionSapMB52Repository
 {
     public function save(InformacionSapMB52 $informacion): bool;
+    public function onGet_By__AlmacenAndPartNumber(string $idAlmacen, string $idPartNumber): ?InformacionSapMB52;
 }
