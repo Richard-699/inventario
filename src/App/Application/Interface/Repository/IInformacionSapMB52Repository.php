57826141e55c45f3b20597efce64ab1e0047e893
ScanoHWI <administrador.smartcenter@hacebwhirlpool.com>
2025-08-15 14:15:12 -0500

    Restauracion archivos mb52

---
+++ src/App/Application/Interface/Repository/IInformacionSapMB52Repository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Application\Interface\Repository;
+
+use App\Domain\Model\InformacionSapMB52;
+
+interface IInformacionSapMB52Repository
+{
+    public function onGet_By__Id_Partnumber($id_partnumber): ?array;
+    public function save(InformacionSapMB52 $informacion): bool;
+    public function onGet_By__AlmacenAndPartNumber(string $idAlmacen, string $idPartNumber): ?InformacionSapMB52;
+}
